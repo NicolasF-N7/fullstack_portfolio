89e8e19070f7ccdb81bf0819184c40f79583f731
Nico <nicolasf-n7@users.noreply.github.com>
2024-01-31 16:40:51 +0100

    Finally built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 import { useState } from 'react';
 import Link from 'next/link';
 
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+/*import { Amplify } from 'aws-amplify';
+import amplifyconfig from '/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);*/
 
 export default function Home() {
   return (
